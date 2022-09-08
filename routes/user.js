715d58c1dbfbe659eaa7d3cf32f8bea73d429f47
timtsai <theblazertim1020@gmail.com>
2022-09-08 17:38:25 +0800

    [docs]Adjust user router

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const User = require('../controllers/user');
 
+router.route('/').get(User.profile);
 router.route('/signup').post(User.signUp);
 router.route('/signin').post(User.signIn);
-router.route('/').get(User.profile);
 router.route('/search').get(User.search);
 
 router.route('/friend').post(User.applyFriend);
